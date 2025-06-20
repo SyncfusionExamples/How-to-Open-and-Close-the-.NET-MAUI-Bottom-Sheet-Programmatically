fd01af521e77b5bbe1a2722ba8a6b9713be82280
AravinthSundaramSF <aravinth.sundaram@syncfusion.com>
2025-06-20 12:25:49 +0530

    Add bottomsheet sample

---
+++ BottomSheetSample/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BottomSheetSample
+{
+    public class Book
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
+        public string Published { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public double Rating { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ BottomSheetSample/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace BottomSheetSample
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void ListView_ItemTapped(object? sender, ItemTappedEventArgs e)
+        {
+            bottomSheet.BottomSheetContent.BindingContext = e.Item;
+            bottomSheet.IsOpen = true;
+        }
+
+        private void CloseBottomSheet(object sender, EventArgs e)
+        {
+            bottomSheet.IsOpen = false;
+        }
+    }
+
+}
